eb5522f5426616ae0d633733cd5e02ba7c65b010
team6101 <wtmcrobotics@wccnet.edu>
2023-02-13 17:57:14 -0500

    add comments

--- src/main/java/frc/robot/InputtedControls.java
+++ src/main/java/frc/robot/InputtedControls.java
@@ -6,13 +6,26 @@ import edu.wpi.first.wpilibj.XboxController;
 
 public class InputtedControls {
 
+    /**
+     * strafe amount on the x-axis within a range between -1 and 1
+     * a positive value goes forward
+     */
     private double x;
+    /**
+     * strafe amount on the y-axis within a range between -1 and 1
+     * a positive value goes left
+     */
     private double y;
+
     private double turnAmount;
     private boolean slowMode;
     private XboxController controller;
     private final double SLOW_MODE_MULTIPLIER = 4;
 
+    /**
+     * @returns strafe amount on the x-axis within a range between -1 and 1
+     *          a positive value goes forward
+     */
     public double getX() {
         return x;
     }
