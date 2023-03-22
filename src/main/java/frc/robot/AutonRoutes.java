17f35b32765caa855fb010d517dd618ec9bc4ee4
team6101 <wtmcrobotics@wccnet.edu>
2023-03-22 18:04:36 -0400

    revert auton distance to using encoders

--- src/main/java/frc/robot/AutonRoutes.java
+++ src/main/java/frc/robot/AutonRoutes.java
@@ -24,11 +24,11 @@ public class AutonRoutes {
         // actions.add(new AutonArmCalibrate(false));
         actions.add(new AutonMoveGribber(GribberState.CLOSING));
         actions.add(new AutonMoveArm(ArmPosition.PLACING_TOP));
-        actions.add(new AutonMoveForward(2, 1.25, navX));
+        actions.add(new AutonMoveForward(2, 1.25));
         actions.add(new AutonWait(0.5));
         actions.add(new AutonMoveGribber(GribberState.OPENING));
 
-        actions.add(new AutonMoveForward(-11, 2, navX));
+        actions.add(new AutonMoveForward(-11, 2));
         actions.add(new AutonMultiAction(new AutonMoveArm(ArmPosition.PICKING_UP),
                 new AutonMoveGribber(GribberState.CLOSING)));
         return actions;
@@ -39,7 +39,7 @@ public class AutonRoutes {
         // actions.add(new AutonArmCalibrate(false));
         actions.add(new AutonMoveGribber(GribberState.CLOSING));
         actions.add(new AutonMoveArm(ArmPosition.PLACING_TOP));
-        actions.add(new AutonMoveForward(2, 1.25, navX));
+        actions.add(new AutonMoveForward(2, 1.25));
         actions.add(new AutonWait(0.5));
         actions.add(new AutonMoveGribber(GribberState.OPENING));
         return actions;
@@ -50,13 +50,13 @@ public class AutonRoutes {
         // actionQueue.add(new AutonArmCalibrate(false));
         actionQueue.add(new AutonMoveGribber(GribberState.CLOSING));
         actionQueue.add(new AutonMoveArm(ArmPosition.PLACING_TOP));
-        actionQueue.add(new AutonMoveForward(2, 1.25, navX));
+        actionQueue.add(new AutonMoveForward(2, 1.25));
         actionQueue.add(new AutonWait(0.5));
         actionQueue.add(new AutonMoveGribber(GribberState.OPENING));
 
-        actionQueue.add(new AutonMoveForward(-1, 2, navX));
+        actionQueue.add(new AutonMoveForward(-1, 2));
         actionQueue.add(new AutonMultiAction(new AutonMoveArm(ArmPosition.FLIP_CONE),
-                new AutonMoveForward(-5, 1.5, navX), new AutonMoveGribber(GribberState.CLOSING)));
+                new AutonMoveForward(-5, 1.5), new AutonMoveGribber(GribberState.CLOSING)));
         actionQueue.add(new AutonBalance(MovementDirection.FORWARDS, robotGyro));
         return actionQueue;
     }
@@ -66,11 +66,11 @@ public class AutonRoutes {
         // actionQueue.add(new AutonArmCalibrate(false));
         actionQueue.add(new AutonMoveGribber(GribberState.CLOSING));
         actionQueue.add(new AutonMoveArm(ArmPosition.PLACING_TOP));
-        actionQueue.add(new AutonMoveForward(2, 1.25, navX));
+        actionQueue.add(new AutonMoveForward(2, 1.25));
         actionQueue.add(new AutonWait(0.5));
         actionQueue.add(new AutonMoveGribber(GribberState.OPENING));
 
-        actionQueue.add(new AutonMoveForward(-1.5, 2, navX));
+        actionQueue.add(new AutonMoveForward(-1.5, 2));
 
         actionQueue.add(new AutonMultiAction(new AutonMoveArm(ArmPosition.PICKING_UP),
                 new AutonMoveGribber(GribberState.CLOSING),
@@ -80,13 +80,13 @@ public class AutonRoutes {
 
     public static ArrayDeque<AutonomousAction> leaveCommunityWhilstFacingWall() {
         ArrayDeque<AutonomousAction> actions = new ArrayDeque<>();
-        actions.add(new AutonMoveForward(-6, 2, navX));
+        actions.add(new AutonMoveForward(-6, 2));
         return actions;
     }
 
     public static ArrayDeque<AutonomousAction> leaveCommunityWhilstFacingEnemySide() {
         ArrayDeque<AutonomousAction> actions = new ArrayDeque<>();
-        actions.add(new AutonMoveForward(6, 2, navX));
+        actions.add(new AutonMoveForward(6, 2));
         return actions;
     }
 }
