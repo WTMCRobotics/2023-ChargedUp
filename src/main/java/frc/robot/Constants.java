9028e9bff3a3b9e36261ed641e89736e14c73490
WTMC Robotics <wtmcrobotics@wccnet.edu>
2023-08-31 17:43:48 -0400

    fomrat

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,14 +6,12 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
 
 public final class Constants {
-        private Constants() {}
+        private Constants() {
+        }
 
-
-        public static final double MOVEMENT_SPEED_DIVISOR = 1.8;
+        public static final double MOVEMENT_SPEED_DIVISOR = 1.0;
         public static final double ROTATION_SPEED_DIVISOR = 1;
         public static final double ARM_SPEED_DIVISOR = 1.3;
-
-
 
         public static final double MAX_ARM_UP_DEGREES = 108;
 
@@ -41,14 +39,16 @@ public final class Constants {
         public static final double ROBOT_SPEED_WHILE_BALANCING_BEFORE_CHARGE_STATION = 0.55;
         public static final double ROBOT_SPEED_WHILE_BALANCING_ON_CHARGE_STATION = .275;
         /**
-         * The amount of time in seconds the robot has to be unbalanced for for it to change to it's
+         * The amount of time in seconds the robot has to be unbalanced for for it to
+         * change to it's
          * "unbalanced" state
          */
         public static final double BEING_UNBALANCED_DEBOUNCE_TIME = .5;
 
         public static final double BALANCING_ACTIVATE_PID_MARGIN_OF_ERROR = 3;
         /**
-         * The amount of time in seconds the robot has to be balanced for for it to be considered
+         * The amount of time in seconds the robot has to be balanced for for it to be
+         * considered
          * balanced
          */
         public static final double BALANCING_DEBOUNCE_TIME = .15;
@@ -64,40 +64,45 @@ public final class Constants {
         public static final float robotLength = 38;
 
         /**
-         * The postition in degrees from the starting position that the arm should go to for placing
+         * The postition in degrees from the starting position that the arm should go to
+         * for placing
          * cubes/cones on the top level
          */
         public static final double ARM_PLACE_TOP_POSITION = 97;
         /**
-         * The postition in degrees from the starting position that the arm should go to for placing
+         * The postition in degrees from the starting position that the arm should go to
+         * for placing
          * cubes/cones on the middle level
          */
         public static final double ARM_PLACE_MIDDLE_POSITION = 60;
         /**
-         * The postition in degrees from the starting position that the arm should go to intake
+         * The postition in degrees from the starting position that the arm should go to
+         * intake
          * cubes
          */
         public static final double ARM_INTAKE_POSITION = 25;
         /**
-         * The postition in degrees from the starting position that the arm should go to for
+         * The postition in degrees from the starting position that the arm should go to
+         * for
          * flipping a cone over
          */
         public static final double ARM_FLIP_CONE_POSITION = 10;
         /**
-         * The postition in degrees from the starting position that the arm should go to for picking
+         * The postition in degrees from the starting position that the arm should go to
+         * for picking
          * up objects
          */
         public static final double ARM_PICK_UP_POSITION = 0;
 
         /**
-         * The position in degrees from the starting position that the arm should go to for picking
+         * The position in degrees from the starting position that the arm should go to
+         * for picking
          * up objects
          */
         public static final double ARM_POSITION_BUFFER_DEGREES = 4;
         // ##########################################
         // Digital IO related constants
         // ##########################################
-
 
         public final static double WHEEL_CIRCUMFERENCE_INCHES = 8 * Math.PI;
         // DIO IDs
@@ -136,17 +141,12 @@ public final class Constants {
         public static double ACCELERATION = 400;
         public static final Gains ARM_GAINS = new Gains(.025, 0.0001, 0.0, 0.0, 0, 1);
         public static final Gains BALANCING_GAINS = new Gains(.015, 0.00001, 0.0, 0.0, 0, .75);
-        public static final Gains PRACTICE_BALANCING_GAINS =
-                        new Gains(.0205, 0.00001, 0.0, 0.0, 0, .75);
+        public static final Gains PRACTICE_BALANCING_GAINS = new Gains(.0205, 0.00001, 0.0, 0.0, 0, .75);
         public static final Gains BUMPERLESS_ROBOT_GAINS = new Gains(0.75, 0, 0, 0, 0, 1);
-        public static final Gains WITH_BUMPER_ROBOT_GAINS =
-                        new Gains(0.35, 0.00001, 100, 0.2, 0, 1.0);
-        public static final Gains BUMPERLESS_ROTATION_GAINS =
-                        new Gains(0.004, 0.003, 0.001, 0.0, 0, 0.0);
-        public static final Gains WITH_BUMPER_ROTATION_GAINS =
-                        new Gains(0.06, 0.003, 0.001, 0.0, 0, 0.0);
-        public static final Constraints ROTATIONAL_GAIN_CONSTRAINTS =
-                        new Constraints(Double.POSITIVE_INFINITY, 20); // m/s
+        public static final Gains WITH_BUMPER_ROBOT_GAINS = new Gains(0.35, 0.00001, 100, 0.2, 0, 1.0);
+        public static final Gains BUMPERLESS_ROTATION_GAINS = new Gains(0.004, 0.003, 0.001, 0.0, 0, 0.0);
+        public static final Gains WITH_BUMPER_ROTATION_GAINS = new Gains(0.06, 0.003, 0.001, 0.0, 0, 0.0);
+        public static final Constraints ROTATIONAL_GAIN_CONSTRAINTS = new Constraints(Double.POSITIVE_INFINITY, 20); // m/s
 
         // and
         // m/s^2
